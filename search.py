6ae976b1c287f1aef3f8b76d75d8caf11f02e0b5
elewashy <elewashy9@gmail.com>
2024-10-22 23:41:23 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,29 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 from urllib.parse import quote, unquote
 
-def search_series(search_query):
-    query_encoded = quote(search_query)  # تشفير الكلمة البحثية
+def search_series(query):
+    query_encoded = quote(query)  # تشفير الاستعلام
     url = f"https://wecima.movie/search/{query_encoded}/"
     response = requests.get(url)
     response.encoding = 'utf-8'  # التأكد من أن الترميز صحيح
     soup = BeautifulSoup(response.content, "html.parser")
-
+    
+    series = []
+    
     series_container = soup.find("div", class_="Grid--WecimaPosts")
-    results = []
-
+    
     if series_container:
         series_elements = series_container.find_all("div", class_="Thumb--GridItem")
-        
+
         for element in series_elements:
             title_element = element.find("strong", class_="hasyear")
             title = title_element.get_text(strip=True) if title_element else "عنوان غير متوفر"
 
             link_element = element.find("a")
             link = link_element["href"] if link_element else "#"
+            
             if link.startswith("https://wecima.movie"):
                 link = link.replace("https://wecima.movie", "")
-
+            
             episode_element = element.find("div", class_="Episode--number")
             episode = episode_element.get_text(strip=True).replace("حلقة", "").strip() if episode_element else None
             
@@ -36,11 +39,13 @@ def search_series(search_query):
                 if image_url_match:
                     image_url = image_url_match.group(1).strip('\"')
 
-            results.append({
+            series.append({
                 "title": title,
+                "link": link,
                 "image_url": image_url,
-                "episode": episode,
-                "link": link
+                "episode": episode
             })
 
-    return results
+    # عرض النص بشكل صحيح
+    print(f"نتائج البحث عن: {unquote(query)}")  # فك تشفير النص هنا
+    return series
